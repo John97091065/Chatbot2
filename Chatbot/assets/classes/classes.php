d1a048c4a9c7279e53b259b4d901bb6a32f616f3
brian <97091876@st.deltion.nl>
2022-06-22 10:26:36 +0200

    wo 22 jun 10:26

--- Chatbot/assets/classes/classes.php
+++ Chatbot/assets/classes/classes.php
@@ -5,4 +5,34 @@ class groups {
         $json = json_decode(file_get_contents("assets/uploads/groups.json"));
         return $json;
     }
+
+    function getGroupByGid(int $GID) {
+
+    }
+
+    function getGroupsByName(string $name) {
+
+    }
+}
+
+class group extends groups {
+
+}
+
+class persons {
+    function getPersonList() {
+
+    }
+
+    function getPersonById() {
+
+    }
+
+    function getPersonByUsername() {
+
+    }
+}
+
+class person extends persons {
+    
 }
--- Chatbot/g.php
+++ Chatbot/g.php
@@ -5,6 +5,8 @@ require_once "assets/classes/classes.php";
 $groups = new groups();
 $groups = $groups->getGroupList();
 
+$group = $groups[1];
+
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +15,36 @@ $groups = $groups->getGroupList();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/group.css">
+    <link rel="stylesheet" href="assets/style/general.css">
     <title><?= strval($groups[0]->groupName) ?></title>
 </head>
 <body>
+    <div class="container">
+        <div class="persons">
+            <header>
+                <p class="fa-solid fa-gear"></p>
+                <p>personen</p>
+                <p>X</p>
+            </header>
+            <div class="wrapper">
+                <div class="pHeader">
+                    <h4>Name</h4>
+                    <h4>role</h4>
+                    <h4>...</h4>
+                </div>
+                <?php for ($i = 0; count($group->persons) > $i; $i++) { ?>
+                <div class="person">
+                    <p class="userName"><?= $group->persons[$i]->Uname ?></p>
+                    <p class="role"><?= $group->persons[$i]->role ?></p>
+                    <p class="Psettings">...</p>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+        <div class="chat">
 
+        </div>
+    </div>
 </body>
 </html>
